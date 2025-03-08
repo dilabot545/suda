33e27479093a0b194901598d23438588faec4e1c
dilabot545 <samannawarathnanawarathna@gmail.com>
2025-03-08 07:57:43 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=T2gTUJxQ#Xe2yEYaSD3OdJJX5OLgg0shRvZV5kI2cRcHAB0w-nsw' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=XuI2BYDQ#0Mlj67SepV8bsoPrZJcbgjnX4fEKpMJVVytm8Tl9hbM' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SUDO: process.env.SUDO === undefined ? '9473737373,9363636367' : process.env.SUDO,
